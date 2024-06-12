3780e14e8a23f369e126c71636e866bffe6d4809
Dominik Ciołczyk <44479446+Dodek69@users.noreply.github.com>
2024-06-12 22:06:42 +0200

    Implement db to streamlit and add config file

---
+++ config.py
@@ -0,0 +1,5 @@
+MODEL_NAME = "mistral-7b-instruct-v0.3.Q8_0:latest"
+
+ES_PORT = 9200
+ES_DISTANCE_STRATEGY = "COSINE"
+ES_INDEX_NAME = "rag"
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,27 @@
 import streamlit as st
 from langchain_core.messages import HumanMessage, AIMessage
-from langchain_core.prompts import ChatPromptTemplate
-from langchain_core.output_parsers import StrOutputParser
 from model_utils import rag_prompt
 from langchain_community.embeddings import OllamaEmbeddings
 from langchain_community.llms.ollama import Ollama
+from config import MODEL_NAME
+from langchain_community.embeddings import OllamaEmbeddings
+from langchain_community.llms.ollama import Ollama
+from db_utils import add_documents_to_db, remove_index, remove_document, fetch_all
+from files_utils import chunk_documents
+from model_utils import rag_prompt
+from langchain_elasticsearch import ElasticsearchStore
+from langchain_community.embeddings import OllamaEmbeddings
+from config import MODEL_NAME, ES_PORT, ES_INDEX_NAME, ES_DISTANCE_STRATEGY
 
-MODEL_NAME = "mistral-7b-instruct-v0.3.Q8_0:latest"
+EMBEDDING = OllamaEmbeddings(model=MODEL_NAME)
+DB_KWARGS = {
+    "embedding": EMBEDDING,
+    "es_url": f"http://localhost:{ES_PORT}",
+    "index_name": ES_INDEX_NAME,
+    "distance_strategy": ES_DISTANCE_STRATEGY,
+}
+DB = ElasticsearchStore(**DB_KWARGS)
+MODEL = Ollama(model=MODEL_NAME)
 
 st.set_page_config(page_title="LLM with RAG system")
 st.title("LLM with RAG system")
@@ -18,8 +33,6 @@ with st.chat_message("assistant"):
 if "chat_history" not in st.session_state:
     st.session_state.chat_history = []
 
-model = Ollama(model=MODEL_NAME)
-    
 # conversation
 for message in st.session_state.chat_history:
     if isinstance(message, HumanMessage):
@@ -41,8 +54,8 @@ if user_query is not None and user_query != "":
         #ai_response = "0111 0110 1110 111 000 [...] - translating to human language -> Napraw mnie....."
         ai_response = rag_prompt(
                     query = user_query,
-                    model=model,
-                    db=db)
+                    model=MODEL,
+                    db=DB)
         st.markdown(ai_response)
         # uncomment after fixing get_response() and delete what is above
         #ai_response = st.write_stream(get_response(user_query, st.session_state.chat_history))
